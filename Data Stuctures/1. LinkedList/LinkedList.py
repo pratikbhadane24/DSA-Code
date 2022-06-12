4e4cebb9ab727d51b3f39e60330bbe53367f8c75
pratikbhadane24 <pratikbhadane24@gmail.com>
2022-06-12 22:34:41 +0530

    added search

--- Data Stuctures/1. LinkedList/LinkedList.py
+++ Data Stuctures/1. LinkedList/LinkedList.py
@@ -1,15 +1,16 @@
 import Node
 
+
 class LinkedList:
     def __init__(self):
         self.head_node = None
-    
+
     def get_head(self):
         return self.head_node
-    
+
     def is_empty(self):
         return self.head_node == None
-    
+
     def print_list(self):
         if self.is_empty():
             return "List is Empty"
@@ -23,8 +24,20 @@ class LinkedList:
     def insert_at_tail(self, value):
         new_node = Node(value)
         temp = self.get_head()
-        
+
         while temp:
             temp = temp.next
         temp.next_element = new_node
         return
+
+    def search(self, value):
+        if self.is_empty():
+            return False
+
+        temp = self.get_head()
+        while temp:
+            if temp.data == value:
+                return True
+            temp = temp.next
+
+        return False
